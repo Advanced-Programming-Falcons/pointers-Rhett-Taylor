7f91a1c03e850e738e7241af5c038a77b1ca9e3b
Rhett-Taylor <157418544+Rhett-Taylor@users.noreply.github.com>
2024-10-07 08:26:15 -0600

    Create realegg.cpp

---
+++ realegg.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <string>
+#include <cmath>
+using namespace std;
+
+int main(void){
+int size;
+cout << "how many numbers?" << endl;
+cin >> size;
+int* array = new int[size];
+for(int i = 0; i < size; i++){
+    array[i] = i*2;
+    cout << array[i] << endl;
+}
+for(int i = 0; i < size; i++){
+    array[i] += 5;
+    cout << array[i] << endl;
+}
+for(int i = size; i < size; i--){
+    array[i] = array[i-1];
+    cout << array[i] << endl;
+}
+cout << array <<": memory location" << endl;
+cout << *array << ": value";
+
+for(int* ptr = array; ptr < array + size; ptr++){
+cout << "Memory address: " << ptr << endl;
+}
+delete[] array;
+return 0;
+}
